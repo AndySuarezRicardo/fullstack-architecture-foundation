509538c174d5802f61415f0e5b5bf6d9960cb63e
AndySuarezRicardo <116969212+AndySuarezRicardo@users.noreply.github.com>
2026-02-10 09:32:19 -0800

    feat(layer-2): frontend/src/routes/index.jsx

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../features/home/Home';
+import UsersPage from '../features/users/UsersPage';
 import NotFound from '../components/common/NotFound';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/users" element={<UsersPage />} />
       <Route path="*" element={<NotFound />} />
-      {/* Add more routes here as modules grow */}
     </Routes>
   );
 };
