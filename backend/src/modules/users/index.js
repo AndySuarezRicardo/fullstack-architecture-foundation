72aa561a09af5d9f805f5d4453fab0a057c1f25d
AndySuarezRicardo <116969212+AndySuarezRicardo@users.noreply.github.com>
2026-02-10 09:32:14 -0800

    feat(layer-2): backend/src/modules/users/index.js

---
+++ backend/src/modules/users/index.js
@@ -0,0 +1,3 @@
+export { default as usersController } from './users.controller.js';
+export { default as usersService } from './users.service.js';
+export { default as usersRoutes } from './users.routes.js';
